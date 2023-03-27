e7dc091bf48d7641fb62a6936e96f4868b7248ec
KobiManuel <Emmakobi91@gmail.com>
2023-03-28 00:55:53 +0100

    feat:

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { useState } from 'react';
 import { parse } from 'svg-parser';
-import './App.css';
+
 
 function App() {
   const [svgCode, setSvgCode] = useState('');
   const [componentCode, setComponentCode] = useState('');
   const [error, setError] = useState('');
+  const [componentName, setComponentName] = useState('SvgComponent');
 
   const handleInputChange = (event) => {
     setSvgCode(event.target.value);
     setComponentCode('');
     setError('');
+  };
+
+  const handleNameChange = (event) => {
+    setComponentName(event.target.value);
   };
 
   const transformSvg = () => {
@@ -25,11 +30,11 @@ function App() {
       // Transform the SVG code into a React component
       const componentCode = `import React from 'react';
 
-const SVGComponent = () => (
+const ${componentName} = () => (
   ${svgCode}
 );
 
-export default SVGComponent;`;
+export default ${componentName};`;
       setComponentCode(componentCode);
     } catch (e) {
       setError(e.message);
@@ -42,12 +47,15 @@ export default SVGComponent;`;
       <p>Paste your SVG code below:</p>
       <textarea rows="10" cols="50" value={svgCode} onChange={handleInputChange}></textarea>
       <br />
+      <label htmlFor="componentName">Component Name:</label>
+      <input type="text" id="componentName" value={componentName} onChange={handleNameChange} />
+      <br />
       <button onClick={transformSvg}>Transform SVG</button>
       {error && <p style={{ color: 'red' }}>{error}</p>}
       {componentCode && (
         <>
           <p>Preview:</p>
-          <pre>{componentCode}</pre>
+          <pre><code>{componentCode}</code></pre>
         </>
       )}
     </div>
