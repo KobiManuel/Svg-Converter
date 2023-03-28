e4d13987efb2e853dcaceed8a2d605cdda6cf3dd
KobiManuel <Emmakobi91@gmail.com>
2023-03-28 16:34:37 +0100

    feat: tested out a rotation animation

--- src/test.js
+++ src/test.js
@@ -1,33 +1,52 @@
-import React from 'react';
-      import PropTypes from 'prop-types';
+import React,{useState} from "react";
+import PropTypes from "prop-types";
 
-      const SvgCop = ({ fill, hoverColor, hoverAnimate}) => {
-        const [filled, setFilled] = useState("green");
-        const [transform, setTransform] = useState("none");
+const SvgCop = ({ fill, hoverColor, hoverAnimate }) => {
+    const [filled, setFill] = useState("green");
+    const [transform, setTransform] = useState("none");
+ return (
+  <svg
+  style={{ transition: "1s", fill: filled, transform: transform }}
+  onMouseEnter={e => {
+    if (hoverColor) {
+      setFill(hoverColor);
+    }
+    if (hoverAnimate) {
+      setTransform("rotate(360deg)");
+    }
+  }}
+  onMouseLeave={e => {
+    setFill(fill);
+    setTransform("none");
+  }}
 
-        return(
-        <svg
-        style={{ transition: "1s", fill: filled, transform: transform }}
-        onMouseEnter={e => {
-          if (hoverColor) {
-            setFilled(hoverColor);
-          }
-          if (hoverAnimate) {
-            setTransform("scale(1.2)");
-          }
-        }}
-        onMouseLeave={e => {
-          setFilled(fill);
-          setTransform("scale(1)");
-        }}
-      width="20" height="19" viewBox="0 0 20 19" fill={fill} xmlns="http://www.w3.org/2000/svg">
-    <path style={{transition: "1s", fill: filled}} onMouseEnter={e => {if (hoverColor) setFill(hoverColor);}} onMouseLeave={e => {setFill(fill);}} fillRule="evenodd" clipRule="evenodd" d="M12.866 12.364C12.72 12.51 12.528 12.584 12.336 12.584C12.143 12.584 11.952 12.51 11.805 12.364L9.999 10.557L8.198 12.358C7.905 12.651 7.43 12.651 7.137 12.358C6.845 12.065 6.845 11.591 7.137 11.298L8.938 9.497L7.136 7.694C6.843 7.401 6.843 6.927 7.136 6.634C7.429 6.341 7.903 6.341 8.196 6.634L9.998 8.436L11.8 6.635C12.093 6.342 12.567 6.342 12.86 6.635C13.153 6.927 13.153 7.402 12.86 7.695L11.059 9.497L12.866 11.303C13.159 11.596 13.159 12.071 12.866 12.364ZM10 0C4.762 0 0.5 4.261 0.5 9.5C0.5 14.738 4.762 19 10 19C15.238 19 19.5 14.738 19.5 9.5C19.5 4.261 15.238 0 10 0Z" fill={fill}/>
-    </svg>
-      )};
-      SvgComponent.propTypes = {
-        fill: PropTypes.string.isRequired,
-        hoverColor: PropTypes.string,
-        hoverAnimate: PropTypes.bool
-      };
+    width="50"
+    height="49"
+    viewBox="0 0 20 19"
+    fill={fill}
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+    style={{ transition: "1s", fill: filled }}
+    onMouseEnter={e => {
+      if (hoverColor) {
+        setFill(hoverColor);
+      }
+    }}
+    onMouseLeave={e => {
+      setFill(fill);
+    }}
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M13.993 7.722L9.375 12.34C9.228 12.487 9.037 12.56 8.845 12.56C8.652 12.56 8.461 12.487 8.314 12.34L6.005 10.031C5.712 9.738 5.712 9.263 6.005 8.97C6.298 8.677 6.772 8.677 7.065 8.97L8.845 10.749L12.932 6.661C13.225 6.368 13.7 6.368 13.993 6.661C14.286 6.954 14.286 7.429 13.993 7.722ZM10 0C4.762 0 0.5 4.262 0.5 9.5C0.5 14.739 4.762 19 10 19C15.238 19 19.5 14.739 19.5 9.5C19.5 4.262 15.238 0 10 0Z"
+      fill={fill}
+    />
+  </svg>
+) };
+SvgCop.propTypes = {
+  fill: PropTypes.string.isRequired,
+  hoverColor: PropTypes.string,
+  hoverAnimate: PropTypes.bool,
+};
 
 export default SvgCop;
