9a5f00774385872be25c32925986d4a4cb4661e5
KobiManuel <Emmakobi91@gmail.com>
2023-04-12 22:26:48 +0100

    feat: converted previous icons to components

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import CopyIcon from "./components/Copy-icon";
 import FailedIcon from "./components/Icons/FailedIcon";
 import UsersIcon from "./components/Icons/UsersIcon";
 import SuccessIcon from "./components/Icons/SuccessIcon";
+import InfoIcon from "./components/Icons/InfoIcon";
 
 function App() {
   const [svgCode, setSvgCode] = useState("");
@@ -747,39 +748,15 @@ export default ${componentName};`;
       <div className="container">
         <div className="inner-container">
           <h2>
-            <svg
-              height="48"
-              viewBox="0 0 48 48"
-              width="48"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M0 0h48v48h-48z" fill="none" />
-              <path d="M22 34h4v-12h-4v12zm2-30c-11.05 0-20 8.95-20 20s8.95 20 20 20 20-8.95 20-20-8.95-20-20-20zm0 36c-8.82 0-16-7.18-16-16s7.18-16 16-16 16 7.18 16 16-7.18 16-16 16zm-2-22h4v-4h-4v4z" />
-            </svg>
+            <InfoIcon />
             Convert Svg icons to React Component
           </h2>
           <p className="info-message">
-            <svg
-              height="48"
-              viewBox="0 0 48 48"
-              width="48"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M0 0h48v48h-48z" fill="none" />
-              <path d="M22 34h4v-12h-4v12zm2-30c-11.05 0-20 8.95-20 20s8.95 20 20 20 20-8.95 20-20-8.95-20-20-20zm0 36c-8.82 0-16-7.18-16-16s7.18-16 16-16 16 7.18 16 16-7.18 16-16 16zm-2-22h4v-4h-4v4z" />
-            </svg>
+          <InfoIcon />
             Props are enabled by default{" "}
           </p>
           <p className="info-message">
-            <svg
-              height="48"
-              viewBox="0 0 48 48"
-              width="48"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M0 0h48v48h-48z" fill="none" />
-              <path d="M22 34h4v-12h-4v12zm2-30c-11.05 0-20 8.95-20 20s8.95 20 20 20 20-8.95 20-20-8.95-20-20-20zm0 36c-8.82 0-16-7.18-16-16s7.18-16 16-16 16 7.18 16 16-7.18 16-16 16zm-2-22h4v-4h-4v4z" />
-            </svg>
+          <InfoIcon />
             Made for icons only
           </p>
           <div className="main">
